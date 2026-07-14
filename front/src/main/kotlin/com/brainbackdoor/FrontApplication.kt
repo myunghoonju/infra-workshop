eb32d343f53132f39e715e59589598fae99a4672
MyungHoon Ju <myunghoonju@gmail.com>
2026-07-14 17:16:01 +0900

    edit for test

--- front/src/main/kotlin/com/brainbackdoor/FrontApplication.kt
+++ front/src/main/kotlin/com/brainbackdoor/FrontApplication.kt
@@ -1,14 +1,22 @@
 package com.brainbackdoor
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [
+        DataSourceAutoConfiguration::class,
+        HibernateJpaAutoConfiguration::class,
+        JpaRepositoriesAutoConfiguration::class,
+    ],
+)
 class FrontApplication
 
 fun main(args: Array<String>) {
@@ -18,7 +26,3 @@ fun main(args: Array<String>) {
 @ConfigurationPropertiesScan
 @Configuration
 class ComponentConfiguration
-
-@EnableJpaAuditing
-@Configuration
-class JpaConfiguration
